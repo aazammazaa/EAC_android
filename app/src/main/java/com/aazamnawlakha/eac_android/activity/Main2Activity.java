573a542fd8fdbb63db84c2b19c22cfb39690d6aa
aazammazaaa <aazammazaa@gmail.com>
2016-08-07 17:18:33 +0530

    constant base url change

--- app/src/main/java/com/aazamnawlakha/eac_android/activity/Main2Activity.java
+++ app/src/main/java/com/aazamnawlakha/eac_android/activity/Main2Activity.java
@@ -2,26 +2,37 @@ package com.aazamnawlakha.eac_android.activity;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aazamnawlakha.eac_android.R;
 
 public class Main2Activity extends Activity {
-    Button mSubmitButton;
+    TextView mTaskForm, mEmpRegistration;
     Context mContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.task_form);
+        setContentView(R.layout.activity_main2);
         mContext = this;
-        mSubmitButton = (Button) findViewById(R.id.submit_Button);
-        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+        mTaskForm = (TextView) findViewById(R.id.submit_Button);
+        mTaskForm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(mContext,"Work in Progress",Toast.LENGTH_LONG).show();
+                Intent mTaskIntent = new Intent(getApplicationContext(),TaskFormActivity.class);
+                startActivity(mTaskIntent);
+                //Toast.makeText(mContext,"Work in Progress",Toast.LENGTH_LONG).show();
+            }
+        });
+        mEmpRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mEmploginIntent = new Intent(getApplicationContext(),EmployeeLogin.class);
+                startActivity(mEmploginIntent);
             }
         });
 
---
+++ app/src/main/java/com/aazamnawlakha/eac_android/activity/TaskFormActivity.java
@@ -0,0 +1,29 @@
+package com.aazamnawlakha.eac_android.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.aazamnawlakha.eac_android.R;
+
+public class TaskFormActivity extends Activity {
+    Button mSubmitButton;
+    Context mContext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.task_form);
+        mContext = this;
+        mSubmitButton = (Button) findViewById(R.id.submit_Button);
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(mContext,"Work in Progress",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
