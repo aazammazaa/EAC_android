110f031d11d0a9506501309f25770643468c9e8a
Husain <husainali01@gmaill.com>
2016-08-07 20:15:08 +0530

    Back Button

--- app/src/main/java/com/aazamnawlakha/eac_android/activity/EmployeeLogin.java
+++ app/src/main/java/com/aazamnawlakha/eac_android/activity/EmployeeLogin.java
@@ -1,9 +1,13 @@
 package com.aazamnawlakha.eac_android.activity;
 
+import android.app.ActionBar;
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,15 +15,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.aazamnawlakha.eac_android.R;
+import com.android.volley.ParseError;
+import com.android.volley.VolleyError;
+
+import org.json.JSONObject;
 
 import java.util.UUID;
 
-public class EmployeeLogin extends AppCompatActivity {
+public class EmployeeLogin extends BaseActivity {
 
-   TextView password_textView;
-    Button generatePassword;
-    Context mContext;
-    boolean isPasswordGenerated = true;
+  private TextView password_textView;
+   private Button generatePassword;
+    private Context mContext;
+    private boolean isPasswordGenerated = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +49,42 @@ public class EmployeeLogin extends AppCompatActivity {
                 isPasswordGenerated = false;
             }
         });
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
+
+    @Override
+    protected String getTagName() {
+        return null;
+    }
+
+    @Override
+    public void showProgress(boolean show, String tag) {
+
+    }
+
+    @Override
+    public void onSuccess(JSONObject response, String tag) {
+
+    }
+
+    @Override
+    public void onError(VolleyError error, String message, String tag) {
+
+    }
+
     public String passwordGenerator(int mPassLenght){
         String uuid = UUID.randomUUID().toString().replaceAll("-", "").trim();
         uuid = uuid.substring(0,mPassLenght);
         Log.i("random password",uuid);
         return  uuid;
+    }
+    public boolean onOptionsItemSelected(MenuItem item){
+        Intent myIntent = new Intent(getApplicationContext(), Main2Activity.class);
+        startActivityForResult(myIntent, 0);
+        return true;
+
     }
 }
