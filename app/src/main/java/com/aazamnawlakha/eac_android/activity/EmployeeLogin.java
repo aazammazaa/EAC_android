3b2560e8fd36c0125850cd4e27d149a9e88472f9
Husain <husainali01@gmaill.com>
2016-08-07 16:07:55 +0530

    Password Generator

--- app/src/main/java/com/aazamnawlakha/eac_android/activity/EmployeeLogin.java
+++ app/src/main/java/com/aazamnawlakha/eac_android/activity/EmployeeLogin.java
@@ -2,17 +2,34 @@ package com.aazamnawlakha.eac_android.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.aazamnawlakha.eac_android.R;
 
 import java.util.UUID;
 
 public class EmployeeLogin extends AppCompatActivity {
 
+   TextView password_textView;
+    Button generatePassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        password_textView = (TextView) findViewById(R.id.password_textView);
+        generatePassword = (Button) findViewById(R.id.generate_button);
+
+        generatePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                password_textView.setText(passwordGenerator(8));
+            }
+        });
     }
     public String passwordGenerator(int mPassLenght){
-
         String uuid = UUID.randomUUID().toString().replaceAll("-", "").trim();
         uuid.substring(0,mPassLenght);
         return  uuid;
